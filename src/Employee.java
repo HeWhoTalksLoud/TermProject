03cd2f7ce7fe7d17f16bb94807dd291966bb8caa
Pasha <nikolenko.pavel@gmail.com>
2022-12-14 20:47:15 +0300

    Изначальный вариант.

---
+++ src/Employee.java
@@ -0,0 +1,50 @@
+public class Employee {
+
+    private static int counter = 0;
+    private String fullName;
+    private int department;
+    private int salary;
+    private int id;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+
+
+    public Employee(String fullName, int department, int salary) {
+        if (fullName.isEmpty()) {
+            throw new IllegalArgumentException("Имя сотрудника не может быть пустым");
+        }
+        this.fullName = fullName;
+        this.id = counter++;
+        setDepartment(department);
+        setSalary(salary);
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        if (department < 1 || department > 5) {
+            throw new IllegalArgumentException("Номер департамента должен быть в диапазоне от 1 до 5");
+        }
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("Значение зарплаты не может быть отрицательным");
+        }
+        this.salary = salary;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Employee[] employees = new Employee[10];
+
+
+        System.out.println(employees[0]);
+    }
+}
